3f492dc8b247cc5ef2ec4fb09ecf1b76bbef8f34
Joshi Shreerang Dhananjay <purplecode.05@gmail.com>
2026-05-08 23:36:43 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        bool carry = true;
+        for(int i = n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                carry = false;
+                break;
+            }
+            else digits[i]=0;
+        }
+        if(carry) digits.insert(digits.begin(),1);
+        return digits;
+
+    }
+};
