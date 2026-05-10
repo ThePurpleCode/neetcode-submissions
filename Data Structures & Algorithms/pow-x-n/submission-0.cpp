1d264514533c4fafae16ed201ae690527cc2d849
Joshi Shreerang Dhananjay <purplecode.05@gmail.com>
2026-05-10 14:19:25 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long long power =n;
+
+        if(power<0){
+            x= 1.0/x;
+            power = -power;
+        }
+        while(power>0){
+            if(power%2==1){
+                ans *=x; // odd power hai toh ek baar multiply kr lo
+            }
+            x *=x;
+            power/=2;
+        }
+        return ans;
+    }
+};
