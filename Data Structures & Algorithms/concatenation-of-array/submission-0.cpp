bdfd0e2822011050a2538c1148ae7ba0d7b753c1
Joshi Shreerang Dhananjay <purplecode.05@gmail.com>
2026-06-02 21:54:33 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(auto num:nums) ans.push_back(num);
+        for(auto num:nums) ans.push_back(num);
+        return ans;
+    }
+};
